d1afc0cfca6e2e77959edea262318fa681502668
falsepopsky <this.is.rodri@outlook.com>
2020-01-23 04:05:57 -0300

    agregado una funcion para la transcripcion del DNA, .gitignore e ingreso manual de DNA por si el .txt no existe

--- ADN.py
+++ ADN.py
@@ -1,49 +1,45 @@
 # El programa toma una secuencia de DNA 3' a 5' y lo traduce a un ARN mensajero 5' a 3'
+import os.path as path
 
-file = open('sample_dna.txt', 'r')
-dna = file.read()
-print ("DNA: 3\'", dna, "5\'")
-rnam = ""
-
-for i in dna:
-    if i == "G":
-        rnam += "C"
-    elif i == "A":
-        rnam += "U"
-    elif i == "T":
-        rnam += "A"
+# Esta función se encarga de traducir 
+def ARNm(dnadata):
+  rnam = ""
+  rna_t = ""
+  for s in dnadata:
+    if s == "G":
+      rnam += "C"
+    elif s == "A":
+      rnam += "U"
+    elif s == "T":
+      rnam += "A"
     else:
-        rnam += "G"
-
-print ("RNAm: 5\'", rnam, "3\'")
-
-rna_t = ""
-
-for i in rnam:
-    if i == "G":
-        rna_t+= "C"
-    elif i == "U":
-        rna_t+= "A"
-    elif i == "A":
-        rna_t+= "U"
+      rnam += "G"
+  print("RNAm: 5\'", rnam, "3\'")
+  for s in rnam:
+    if s == "G":
+      rna_t+= "C"
+    elif s == "U":
+      rna_t+= "A"
+    elif s == "A":
+      rna_t+= "U"
     else:
-        rna_t+= "G"
-
-print ("RNAt: 3\'", rna_t, "5\'")
-
-
-#def dnatoRNAm(a,t,c,g)
-#    if i == "a"
+      rna_t+= "G"
+  print ("RNAt: 3\'", rna_t, "5\'")
+  return ""
 
 
-#def rnamtoRNAt()
-
-
-#Transcripcion de DNA 5' a 3' a ARNm 3' a 5'
-
-
-
-
+ifile = ('sample_dna.txt')
+if path.exists('sample_dna.txt'):
+    a = open(ifile, 'r')
+    adn_saved = a.read()
+    print ("DNA: 3\'", adn_saved, "5\'")
+    adn = ARNm(adn_saved)
+else:
+    print("No hay muestra")
+    data = str(input("Introduzca la secuencia genetica 3\' a 5\'"))
+    print ("DNA: 3\'", data, "5\'")
+    x = ARNm(data)
+    print (x)
 
 #Transcripcion de ARNm a aminoacidos
 
